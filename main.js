22ed6c5691beb77c34363fb729d53eb6dfa986bd
Kath Ling <talk2keling@gmail.com>
2020-02-29 13:12:24 -0500

    loops

--- main.js
+++ main.js
@@ -215,15 +215,79 @@
 
 
 /*
-   __              _                       
-  / _| ___  _ __  | | ___   ___  _ __  ___ 
- | |_ / _ \| '__| | |/ _ \ / _ \| '_ \/ __|
- |  _| (_) | |    | | (_) | (_) | |_) \__ \
- |_|  \___/|_|    |_|\___/ \___/| .__/|___/
-                                |_|        
+  _                       
+ | | ___   ___  _ __  ___ 
+ | |/ _ \ / _ \| '_ \/ __|
+ | | (_) | (_) | |_) \__ \
+ |_|\___/ \___/| .__/|___/
+               |_|            
 */
+// // for loops
+// for(let i = 0; i< 10; i++) {
+//     console.log(`For Loop Number: ${i}`);
+// }
 
-for(let i = 0; i< 10; i++) {
-    console.log(`For Loop Number: ${i}`);
-}
+
+// // while loops
+// let i = 0;
+// while(i < 10) {
+//     console.log(`While Loop Number: ${i}`);
+//     i++;
+// }
+
+// // looping through an array
+// const todos = [
+//     {
+//         id: 1,
+//         text: 'Take out trash',
+//         isCompleted: true
+//     },
+//     {
+//         id: 2,
+//         text: 'Meeting with boss',
+//         isCompleted: true
+//     },
+//     {
+//         id: 3,
+//         text: 'Dentist appt',
+//         isCompleted: false
+//     }
+// ];
+
+// for(let i = 0; i< todos.length; i++) {
+//     console.log(todos[i].text);
+// }
+
+// for(let todo of todos) {
+//     console.log(todo.text);
+// }
+
+// for(const {text, id, x} of todos) {
+//     console.log(id, text, x);
+// }
+
+// high order array methods, iteration
+// forEach, map, filter
+// todos.forEach(function(todo) {
+//     console.log(todo.text);
+// });
+
+// const todoText = todos.map(function(todo) {
+//     return todo.text;
+// });
+// console.log(todoText);
+
+// const todoCompleted = todos.filter(function(todo) {
+//     return todo.isCompleted === true;
+// });
+// console.log(todoCompleted);
+
+// method chaining
+// const todoCompleted = todos.filter(function(todo) {
+//     return todo.isCompleted === true;
+// }).map(function(todo){
+//     return todo.text;
+// })
+
+// console.log(todoCompleted);
 
